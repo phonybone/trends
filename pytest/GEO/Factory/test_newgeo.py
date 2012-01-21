7ee7db3638379e6ce790c02219dcc1edb75b00a2
Victor Cassen <phonybone@gmail.com>
2012-01-21 14:20:52 -0800

    Factory passing tests

--- pytest/GEO/Factory/test_newgeo.py
+++ pytest/GEO/Factory/test_newgeo.py
@@ -1,14 +1,10 @@
-import unittest, sys, os, re
+import unittest, sys, os, yaml
 sys.path.append(os.path.join(os.environ['AUREA_HOME'], 'src'))
 sys.path.append(os.path.join(os.environ['TRENDS_HOME'], 'pylib'))
 
-
 from warn import *
-from GEO import Factory
+from GEO.Factory import Factory
 from GEO.Series import Series
-from GEO.Sample import Sample
-from GEO.Dataset import Dataset
-from GEO.Platform import Platform
 
 class TestNewGEO(unittest.TestCase):
     
@@ -16,11 +12,12 @@ class TestNewGEO(unittest.TestCase):
         pass
         
     def test_new_geo(self):
-        f=Factory.Factory()
-        geo_id='GSE00001'
+        f=Factory()
+        geo_id='GSE10072'
         s=f.newGEO(geo_id)
         self.assertIsInstance(s, Series)
-
+        self.assertEqual(s.geo_id, 'GSE10072')
+#        warn("series is %s" % yaml.dump(s))
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestNewGEO)
